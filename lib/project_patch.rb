6736f17a47651642a67ddd911c71425b4922e94f
Anton Shubin <signal2564@gmail.com>
2013-08-14 19:14:04 +0700

    deep copy for project

--- lib/project_patch.rb
+++ lib/project_patch.rb
@@ -3,7 +3,9 @@ require_dependency 'project'
 
 # Patches Redmine's Project dynamically.
 module ProjectPatch
-  def self.included(base) # :nodoc:
+  def self.included(base) # :nodoc
+	  base.send(:include, InstanceMethods)
+	  
      # Same as typing in the class
     base.class_eval do
       belongs_to    :template, class_name: 'Project'
@@ -18,3 +20,13 @@ end
 # Add module to Project
 Project.send(:include, ProjectPatch)
 Project.safe_attributes 'is_template', 'start_date'
+
+module InstanceMethods
+	def copy_from_with_deep_copy(project)
+		copy = copy_from(project)
+		copy.issues = project.issues
+		copy.news = project.news
+		copy.documents = projects.documents
+		copy
+	end
+end
