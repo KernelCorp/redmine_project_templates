ffbebd6ba8231663de628d9ce110b3f9699bf13a
max-konin <qwas666>
2013-08-15 12:23:35 +0700

    add locales for template buttom on top menu. fix project patch

--- lib/project_patch.rb
+++ lib/project_patch.rb
@@ -15,18 +15,19 @@ module ProjectPatch
 
   end
 
+  module InstanceMethods
+    def copy_from_with_deep_copy(project)
+      copy = copy_from(project)
+      copy.issues = project.issues
+      copy.news = project.news
+      copy.documents = projects.documents
+      copy
+    end
+  end
+
 end
 
 # Add module to Project
 Project.send(:include, ProjectPatch)
 Project.safe_attributes 'is_template', 'start_date'
 
-module InstanceMethods
-	def copy_from_with_deep_copy(project)
-		copy = copy_from(project)
-		copy.issues = project.issues
-		copy.news = project.news
-		copy.documents = projects.documents
-		copy
-	end
-end
